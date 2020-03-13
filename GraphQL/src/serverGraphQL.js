dec16dcfad92f0adc50f739dac170e83935df3b5
dwolpers <david.wolpers@seitenbau.com>
2020-03-13 11:23:41 +0100

    removed zeus

--- GraphQL/src/serverGraphQL.js
+++ GraphQL/src/serverGraphQL.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var { buildSchema  } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+var schemaV1 = buildSchema(`
   type Query {
-    allBooks(title: String): [Book]
+    allBooks(title: String): [Book],
+    rollDice(numDice: Int!, numSides: Int): [Int]
+    
+  },
+  type Book {
+    id: Int
+    author: String
+    title: String
+    url2:String @deprecated(reason: "Use url")
+    url:String
+  }
+`);
+
+var schemaV2 = buildSchema(`
+  type Query {
+    allBooks(title: String): [Book],
+    rollDice(numDice: Int!, numSides: Int): [Int]
+    
   },
   type Book {
     id: Int
@@ -33,11 +50,16 @@ var bookData = [
         title: 'JavaScript: Understanding The Weird Parts',
         author: 'Anthony Alicea',
         url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    },
+    {
+        id: 4,
+        title: 'The JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
     }
 ];
 
 var getAllBooks = function (filter) {
-    console.log(filter);
     if (filter.title) {
         var title = filter.title;
         return bookData.filter(book => book.title.startsWith(title))
@@ -45,16 +67,34 @@ var getAllBooks = function (filter) {
     return bookData;
 };
 
+var rollDices = function (args) {
+    var output = [];
+    for (var i = 0; i < args.numDice; i++) {
+        output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    }
+    return output;
+};
+
 // The root provides a resolver function for each API endpoint
 var root = {
-    allBooks: getAllBooks
+    allBooks: getAllBooks,
+    rollDice: rollDices
 };
 
 var app = express();
 app.use('/', graphqlHTTP({
-    schema: schema,
+    schema: schemaV1,
     rootValue: root,
     graphiql: true,
 }));
 app.listen(3000);
 console.log('Running a GraphQL API server at http://localhost:3000/');
+
+var app2 = express();
+app2.use('/', graphqlHTTP({
+    schema: schemaV2,
+    rootValue: root,
+    graphiql: true,
+}));
+app2.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/');
