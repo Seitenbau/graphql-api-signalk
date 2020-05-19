51337a1910dbe2fdadd7d8fab1d4acfefa33b0a7
da721wol <da721wol@gmx.de>
2020-05-19 14:19:33 +0200

    Anbindung der GraphQL Routenschnittstelle #13

--- Code/GraphQLClient/graphQLClient/routes/index.js
+++ Code/GraphQLClient/graphQLClient/routes/index.js
@@ -5,18 +5,26 @@ var fetch = require('node-fetch');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getRoute', function(req, res, next) {
+router.get('/getRoute', function (req, res, next) {
   fetch('http://localhost:3000/signalk/v1/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "query { route(uuid: \"3f441c99-67c1-48cd-8e97-9db483621eff\") { uuid, name } }"})
+    body: JSON.stringify({
+      query: `
+      query { 
+        route(uuid: "3f441c99-67c1-48cd-8e97-9db483621eff") { 
+          uuid, 
+          name 
+        } 
+      }
+      ` })
   })
     .then(r => r.json())
     .then(data => {
@@ -25,14 +33,22 @@ router.get('/getRoute', function(req, res, next) {
     });
 });
 
-router.get('/getRoutes', function(req, res, next) {
+router.get('/getRoutes', function (req, res, next) {
   fetch('http://localhost:3000/signalk/v1/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "query { routes { uuid, name }}"})
+    body: JSON.stringify({
+      query: `
+      query { 
+        routes { 
+          uuid, 
+          name 
+        }
+      }
+    `})
   })
     .then(r => r.json())
     .then(data => {
