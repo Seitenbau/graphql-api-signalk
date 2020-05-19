a4ea7f61a2525752e3e6500162a4ebfe55743c97
da721wol <da721wol@gmx.de>
2020-05-19 12:56:11 +0200

    Anbindung der GraphQL Routenschnittstelle #13

--- Code/GraphQLClient/graphQLClient/routes/index.js
+++ Code/GraphQLClient/graphQLClient/routes/index.js
@@ -5,18 +5,28 @@ var fetch = require('node-fetch');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
+router.get('/test', function (req, res, next) {
   fetch('http://localhost:3000/signalk/v1/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "{ hello }"})
+    body: JSON.stringify({
+      query: "mutation { " +
+        " addRoute(distance: 2.4, feature: { " +
+        " geometry: { " +
+        " coordinates: [[123.4, 212.5], [123.12, 222.41], [123.2, 332.1]] " +
+        " } " +
+        " }, " +
+        " name:\"Test\") { " +
+        " uuid, " +
+        " name}}"
+    })
   })
     .then(r => r.json())
     .then(data => {
