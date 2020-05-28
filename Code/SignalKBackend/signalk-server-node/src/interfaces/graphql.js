d8523021eb178bcea386ce23f8d9f0ecc3192791
da721wol <da721wol@gmx.de>
2020-05-28 11:10:56 +0200

    New Database #19

--- Code/SignalKBackend/signalk-server-node/src/interfaces/graphql.js
+++ Code/SignalKBackend/signalk-server-node/src/interfaces/graphql.js
@@ -15,19 +15,10 @@
  * limitations under the License.
  */
 
-var express = require('express');
+var setupDatabase = require('./database');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
-
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const adapter = new FileSync('./database/db.json')
-const db = low(adapter)
-
-
 var fs = require('fs');
-var app = express();
 
 module.exports = function (app) {
     'use strict';
@@ -36,19 +27,13 @@ module.exports = function (app) {
     const versionPrefix = '/v1';
     const apiPathPrefix = pathPrefix + versionPrefix + '/graphql';
 
-    db.defaults({ routes: [] })
-        .write()
-
-    db.get('routes')
-        .push({ uuid: "3f441c99-67c1-48cd-8e97-9db483621eff", distance: 6571.329987262973, feature: { type: "Feature", geometry: { type: "LineString", coordinates: [[23.395420074462887, 59.916457640310426], [23.456703186035153, 59.917662281506665], [23.50820159912109, 59.9293622351139]] }, id: "" }, start: null, end: null, description: "Hallo", name: "Test4", timestamp: null, source: "Test" })
-        .write()
+    var db = setupDatabase();
 
     var resourcePath = './resources/routes';
 
     var schema = buildSchema(`
         type Query {
             hello(name: String!): String,
-            routes: [Route],
             route(uuid: String, name: String): Route
         },
 
@@ -80,21 +65,43 @@ module.exports = function (app) {
     var root = {
         hello: () => "Hello World!",
 
-        routes() {
-            var routes = db.get('routes').value();
-            return routes;
-        },
-
-        route(args) {
+        async route(args) {
+            let sql = `
+                SELECT route_id,uuid,distance,start,end,description,name,timestamp,source, feature.type feature_type, feature.id, geometry.type geometry_type, geometry.coordinates
+                FROM routes
+                INNER JOIN feature on routes.feature_id = feature.feature_id
+                INNER JOIN geometry on feature.geometry_id = geometry.geometry_id
+                `
+            var param;
             if (args.uuid) {
-                return db.get('routes')
-                    .find({ uuid: args.uuid })
-                    .value()
+                param = args.uuid;
+                sql = sql + `WHERE uuid = ?; `
+            } else if(args.name) {
+                param = args.name;
+                sql = sql + `WHERE name = ?; `
             } else {
-                return db.get('routes')
-                    .find({ name: args.name })
-                    .value()
+                return;
             }
+            var result = new Promise((resolve, reject) => {
+                db.get(sql, [param], (err, res) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(res);
+                });
+            });
+            var row = await result;
+            if(row) {
+                row.feature = {
+                    id: row.id,
+                    type: row.feature_type,
+                    geometry: {
+                        type: row.geometry_type,
+                        coordinates: JSON.parse(row.coordinates)
+                    }
+                }
+            }
+            return row;
         }
     };
     return {
